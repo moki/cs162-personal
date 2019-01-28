74e95e97fa8470d6a485a30085ccdb458bb28f84
Kirill Morozo (moki) <morozov.kirill.moki@gmail.com>
2019-01-28 01:13:11 +0000

    add 3.4 user limits

--- hw0/main.c
+++ hw0/main.c
@@ -3,9 +3,30 @@
 
 int main() {
 	struct rlimit lim;
-	printf("stack size: %ld\n", 0L);
-	printf("process limit: %ld\n", 0L);
-	printf("max file descriptors: %ld\n", 0L);
+
+	int err = getrlimit(RLIMIT_STACK, &lim);	
+
+	if (err == -1) {
+		printf("error finding out stack size\n");
+	}
+ 
+	printf("stack size: %ld\n", lim.rlim_cur);
+
+	err = getrlimit(RLIMIT_NPROC, &lim);
+
+	if (err == -1) {
+		printf("error finding out max # of threads\n");
+	}
+
+	printf("process limit: %ld\n", lim.rlim_cur);
+
+	err = getrlimit(RLIMIT_NOFILE, &lim);
+
+	if (err == -1) {
+		printf("error finding out max # of fds\n");
+	}
+
+	printf("max file descriptors: %ld\n", lim.rlim_cur);
 
 	return 0;
 }

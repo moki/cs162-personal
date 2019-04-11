3e281568babdef10731a83ae5d1bbf74a67b149f
Morozov Kirill (moki) <morozov.kirill.moki@gmail.com>
2019-04-11 12:51:23 +0000

    add hw2 static files serving functionality

--- hw2/httpserver.c
+++ hw2/httpserver.c
@@ -54,16 +54,278 @@ void handle_files_request(int fd) {
    */
 
   struct http_request *request = http_request_parse(fd);
+  uint16_t resp_status_code;
+  char *resp_mime_type;
+  char *resp_length;
+  char *resp_str;
+  size_t response_size_bytes = 0;
 
-  http_start_response(fd, 200);
-  http_send_header(fd, "Content-Type", "text/html");
+  /* absolute server root path */
+  char *path_buf = NULL;
+  if (!(path_buf = getcwd(NULL, 0))) {
+	perror("getcwd");
+	exit(EXIT_FAILURE);
+  }
+
+  /* static files directory */
+
+  /* cwd +1 for a "/", +1 for null termination */
+  char *req_file_path = calloc(strlen(path_buf) + strlen(server_files_directory) + strlen(request->path) + 1 + 1, sizeof(char));
+  strcat(req_file_path, path_buf);
+  strcat(req_file_path, "/");
+  strcat(req_file_path, server_files_directory);
+  strcat(req_file_path, request->path);
+  free(path_buf);
+
+  struct stat req_file;
+  if (stat(req_file_path, &req_file) == -1) {
+	if (errno == ENOENT) {
+		free(req_file_path);
+		goto not_found;
+	}
+
+	perror("stat");
+	exit(EXIT_FAILURE);
+  }
+
+  if (S_ISDIR(req_file.st_mode)) {
+	// directory request
+	// check if there is index.html serve it if thats the case
+	if (!(req_file_path = realloc(req_file_path,
+					strlen(req_file_path) +
+					strlen("index.html") * sizeof(char))))
+	{
+		perror("realloc req_file_path");
+		exit(EXIT_FAILURE);
+	}
+
+	strcat(req_file_path, "index.html");
+	int index_fd = open(req_file_path, O_RDONLY);
+	if (index_fd == -1) {
+		if (errno == ENOENT) {
+			goto show_dir;
+		}
+		perror("open fd@index.html");
+		exit(EXIT_FAILURE);
+	}
+
+	char *index_html_str = malloc(sizeof(char));
+	size_t html_length = 1;
+	size_t i = 0;
+	char rbuf;
+	ssize_t n;
+
+	while ((n = read(index_fd, &rbuf, 1) > 0)) {
+		if (html_length <= i) {
+			html_length <<= 1;
+			index_html_str = realloc(index_html_str, html_length);
+			if (!index_html_str) {
+				perror("realloc@index_html_str");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		index_html_str[i++] = rbuf;
+	}
+
+	if (html_length <= i) {
+		index_html_str = realloc(index_html_str, html_length + 1);
+		if (!index_html_str) {
+			perror("realloc@index_html_str");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	index_html_str[i++] = '\0';
+
+	i = strlen(index_html_str);
+	resp_str = index_html_str;
+	size_t j = snprintf(NULL, 0, "%lu", i);
+	resp_length = calloc(j + 1, 1);
+	if (!resp_length) {
+		perror("malloc@resp_length");
+		exit(EXIT_FAILURE);
+	}
+	size_t k = snprintf(resp_length, j + 1, "%lu", i);
+	if (j != k) {
+		perror("setting content length");
+		exit(EXIT_FAILURE);
+	}
+	resp_status_code = 200;
+	resp_mime_type = http_get_mime_type(req_file_path);
+	free(req_file_path);
+	close(index_fd);
+	goto respond;
+
+show_dir:;
+	size_t nl = strlen(req_file_path) - strlen("index.html") * sizeof(char);
+	req_file_path = realloc(req_file_path, nl);
+	if (!req_file_path) {
+		perror("realloc@req_file_path");
+		exit(EXIT_FAILURE);
+	}
+	req_file_path[nl] = '\0';
+
+	DIR *dir = opendir(req_file_path);
+	if (!dir) {
+		if (errno == ENOENT) {
+			free(req_file_path);
+			return;
+		}
+
+		perror("opendir@req_file_path");
+		exit(EXIT_FAILURE);
+	}
+
+	struct dirent *dir_entry;
+	char *html_str = calloc(1, 1);
+	char *link = calloc(1, 1);
+	char *link_template = "<a style=\"display: block;\" href=\"%s\">%s</a>";
+
+	while ((dir_entry = readdir(dir))) {
+		char *den = dir_entry->d_name;
+		size_t link_length = strlen(link) + strlen(den) * 2 +
+				     strlen(link_template) - 4;
+		link = realloc(link, link_length);
+		if (!link) {
+			perror("realloc@link");
+			exit(EXIT_FAILURE);
+		}
+		snprintf(link, link_length + 1, link_template, den, den);
+
+		html_str = realloc(html_str, strlen(html_str) + strlen(link));
+		if (!html_str) {
+			perror("realloc@html_str");
+			exit(EXIT_FAILURE);
+		}
+
+		strcat(html_str, link);
+	}
+
+	resp_status_code = 200;
+	resp_mime_type = http_get_mime_type(".html");
+	resp_str = html_str;
+	size_t m = strlen(html_str);
+	size_t l = snprintf(NULL, 0, "%lu", m);
+	resp_length = calloc(l + 1, 1);
+	if (!resp_length) {
+		perror("malloc@resp_length");
+		exit(EXIT_FAILURE);
+	}
+	size_t o = snprintf(resp_length, l + 1, "%lu", m);
+	if (o != l) {
+		perror("setting content length");
+		exit(EXIT_FAILURE);
+	}
+
+	free(link);
+	free(req_file_path);
+	goto respond;
+  } else if (S_ISREG(req_file.st_mode)) {
+	// regular file request
+	int file_fd = open(req_file_path, O_RDONLY);
+	if (file_fd == -1) {
+		if (errno == ENOENT) {
+			goto not_found;
+		}
+		perror("open fd@index.html");
+		exit(EXIT_FAILURE);
+	}
+
+	char *file_str = malloc(sizeof(char));
+	size_t file_str_length = 1;
+	size_t i = 0;
+	char rbuf;
+	ssize_t n;
+
+	while ((n = read(file_fd, &rbuf, 1) > 0)) {
+		if (file_str_length <= i) {
+			file_str_length <<= 1;
+			file_str = realloc(file_str, file_str_length);
+			if (!file_str) {
+				perror("realloc@file_str");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		file_str[i++] = rbuf;
+	}
+
+	response_size_bytes = i;
+	resp_str = file_str;
+	size_t j = snprintf(NULL, 0, "%lu", i);
+	resp_length = calloc(j + 1, 1);
+	if (!resp_length) {
+		perror("malloc@resp_length");
+		exit(EXIT_FAILURE);
+	}
+	size_t k = snprintf(resp_length, j + 1, "%lu", i);
+	if (j != k) {
+		perror("setting content length");
+		exit(EXIT_FAILURE);
+	}
+	resp_status_code = 200;
+	resp_mime_type = http_get_mime_type(req_file_path);
+	free(req_file_path);
+	close(file_fd);
+	goto respond;
+  } else {
+	char *not_allowed = "<h1 style=\"text-align: center;\">405 Not Allowed</h1>";
+
+	resp_status_code = 405;
+	resp_mime_type = http_get_mime_type(".html");
+	resp_str = not_allowed;
+
+	size_t m = strlen(not_allowed);
+	size_t l = snprintf(NULL, 0, "%lu", m);
+	resp_length = calloc(l + 1, 1);
+	if (!resp_length) {
+		perror("malloc@resp_length");
+		exit(EXIT_FAILURE);
+	}
+	size_t o = snprintf(resp_length, l + 1, "%lu", m);
+	if (o != l) {
+		perror("setting content length");
+		exit(EXIT_FAILURE);
+	}
+
+	goto respond;
+  }
+
+not_found:;
+  char *template = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
+  char *not_found = calloc(strlen(template), 1);
+  strcpy(not_found, template);
+  resp_status_code = 404;
+  resp_mime_type = http_get_mime_type(".html");
+  resp_str = not_found;
+
+  size_t m = strlen(not_found);
+  size_t l = snprintf(NULL, 0, "%lu", m);
+  resp_length = calloc(l + 1, 1);
+  if (!resp_length) {
+	perror("malloc@resp_length");
+	exit(EXIT_FAILURE);
+  }
+  size_t o = snprintf(resp_length, l + 1, "%lu", m);
+  if (o != l) {
+	perror("setting content length");
+	exit(EXIT_FAILURE);
+  }
+
+respond:
+
+  http_start_response(fd, resp_status_code);
+  http_send_header(fd, "Content-Type", resp_mime_type);
+  http_send_header(fd, "Content-Length", resp_length);
   http_end_headers(fd);
-  http_send_string(fd,
-      "<center>"
-      "<h1>Welcome to httpserver!</h1>"
-      "<hr>"
-      "<p>Nothing's here yet.</p>"
-      "</center>");
+  if (response_size_bytes)
+	http_send_data(fd, resp_str, response_size_bytes);
+  else
+	http_send_string(fd, resp_str);
+  free(resp_str);
+  free(resp_length);
+  response_size_bytes = 0;
 }
 
 
